6aee92cdf0aa7d9b0ef251060927bec25cc2f554
maiphuong30 <maiphuong30.12.2000@gmail.com>
2021-04-09 15:09:44 +0700

    4/9 change

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
-var app = express();
 var router = express.Router();
+var port = 3000;
 
-
+var app = express();
 app.set('view engine', 'ejs');
-app.set("views","./views")
+app.set("views", "./views")
 app.use(express.static("public"));
 
-var home = require('./routers/pageRouter');
+var home = require('./routes/pageRoute');
+var adRoute = require('./routes/quanliRoute');
 
 app.use('/', home);
+app.use('/admin', adRoute);
 
-app.listen(3000);
+app.listen(port, function () {
+    console.log("Server listening on port " + port);
+});
