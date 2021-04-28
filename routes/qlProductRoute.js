4308a7950b6fb3f5873c8f58a5e038c47282efcd
maiphuong30 <maiphuong30.12.2000@gmail.com>
2021-04-28 21:01:28 +0700

    up: 28/04/21, edit chua hoan thanh

--- routes/qlProductRoute.js
+++ routes/qlProductRoute.js
@@ -6,11 +6,11 @@ var ctl = require("../controllers/admin.controller");
 var sp = require("../models/sanpham");
 
 router.get('/', function (req, res) {
-    sp.find(function(err, data){
-        if(err){
+    sp.find(function (err, data) {
+        if (err) {
             res.json(err);
         }
-        else{
+        else {
             res.render('adminpage/qlsanpham', { title: "Quan li san pham", danhsach: data });
         }
     });
@@ -18,8 +18,20 @@ router.get('/', function (req, res) {
 //Them
 
 router.get('/add', function (req, res) {
-    res.render('adminpage/add', { title: "Them san pham" });
+    res.render('adminpage/add', { title: "Them san pham"});
 });
 router.post('/add', ctl.savetodb);
+router.get('/edit/:id', function (req, res) {
+    sp.findById(req.params.id, function (err, data) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            console.log(data)
+            res.render('adminpage/edit', { title: "Sua san pham", form: "./edit", sanpham: data });
+        }
+    });
+});
+router.post('/edit');
 
 module.exports = router;
