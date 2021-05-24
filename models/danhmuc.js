df4df66611dddea69429be63b06f2fb45eb2839c
thanhson3133 <72431446+thanhson3133@users.noreply.github.com>
2021-05-24 22:23:36 +0700

    update frontend--- loi fe danh muc

--- models/danhmuc.js
+++ models/danhmuc.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var schemaDm = new mongoose.Schema({
     Title: String,
-    //List: [{type: mongoose.Schema.Types.ObjectId}]
+    List: [{type: mongoose.Schema.Types.ObjectId}]
 });
 
 module.exports = mongoose.model("danhmuc", schemaDm);
