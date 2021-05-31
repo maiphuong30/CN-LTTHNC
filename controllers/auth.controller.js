787e12b8872882a5c8f6428ce470d77a9f044093
maiphuong30 <maiphuong30.12.2000@gmail.com>
2021-05-31 15:39:04 +0700

    login user: admin pass: admin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,20 @@
+var acc = require("../models/user");
 module.exports.login = function (req, res) {
     res.render('adminpage/login', { title: "Login"});
 }
 module.exports.postLogin = function (req, res) {
     var username = req.body.username;
-    if(username=='phuong'){
-        res.cookie('user', username);
-        res.redirect('/admin');
-    }
+    var password = req.body.inputPassword;
+    acc.findOne({
+        Username: username,
+        Password: password
+    })
+    .then(user=>{
+        if(user){
+            res.cookie('user', username);
+            res.redirect('/admin');
+        }else{
+            res.json({message: "no user found"});
+        }
+    })
 }
